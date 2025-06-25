8b865661282e3722bf1187492c0cbaf074ea6f21
Derick Ruiz <derick.r.ruiz@gmail.com>
2025-06-25 11:38:01 +0000

    Generating the APIs for the sim idx app.

--- apis/src/db/schema/Listener.ts
+++ apis/src/db/schema/Listener.ts
@@ -1,5 +1,14 @@
-import { pgTable as table } from "drizzle-orm/pg-core";
-import { db } from "sim-idx";
+
+// Generated file. Do not edit manually.
+import { pgEnum, pgTable as table } from "drizzle-orm/pg-core";
+import * as t from "drizzle-orm/pg-core";
+import { db, types } from "sim-idx";
+
+export const ownerQueried = table("owner_queried", {
+  chainId: db.uint64('chain_id'),
+  owner: db.address('owner'),
+  blockNumber: db.uint256('block_number'),
+})
 
 export const poolCreated = table("pool_created", {
   chainId: db.uint64('chain_id'),
@@ -8,4 +17,15 @@ export const poolCreated = table("pool_created", {
   token0: db.address('token0'),
   token1: db.address('token1'),
   fee: db.uint24('fee'),
+  blockNumber: db.uint256('block_number'),
+})
+
+export const sushiswapPairCreated = table("sushiswap_pair_created", {
+  chainId: db.uint64('chain_id'),
+  caller: db.address('caller'),
+  pair: db.address('pair'),
+  token0: db.address('token0'),
+  token1: db.address('token1'),
+  allPairsLength: db.uint256('all_pairs_length'),
+  blockNumber: db.uint256('block_number'),
 })
